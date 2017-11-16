0455d2f33712e65d05a7110dac41c9f11d12518b
cherkavi <technik7job@gmail.com>
2017-11-16 07:16:43 +0100

    example of Maven/Gradle for python ( and windows/unix installer )

---
+++ standart-build-tool/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name = "scrabber",
+    packages = ["scrabber"],
+    version = "1.0.0",
+    description = "Just example of the project",
+    author = "Vitalii Cherkashyn",
+    author_email = "technik7job@gmail.com",
+    # url = "http://chardet.feedparser.org/",
+    # download_url = "http://chardet.feedparser.org/download/python3-chardet-1.0.1.tgz",
+    keywords = ["encoding", "i18n", "xml"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Linguistic",
+        ],
+    long_description = """\
+this is long description for this project
+and additional explanation for project
+"""
+)
