30b8827e7375188c9b2e462f3c36a52aad570fde
cherkavi <technik7job@gmail.com>
2017-12-03 19:41:14 +0100

    spark obtain context example

---
+++ spark/spark-test-connection.py
@@ -0,0 +1,12 @@
+from pyspark import SparkContext
+
+#sc = SparkContext("172.17.0.2", "test-app")
+#sc = SparkContext("spark://172.17.0.2", "test-app")
+#SparkConf().setAppName("App_Name").setMaster("spark://localhost:18080").set("spark.ui.port","18080");
+
+try:
+    sc = SparkContext("local", "test-app")
+    print(sc)
+finally:
+    sc.stop()
+
