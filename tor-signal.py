15d969b5abbc20cd4812958f6d66032f4b3bb9fe
Vitalii <technik7job@gmail.com>
2017-12-19 17:27:20 +0200

    using tor network to obtain another ip address

--- tor-signal.py
+++ tor-signal.py
@@ -7,14 +7,23 @@
 #
 #  sudo service tor restart  (or)  sudo /etc/init.d/tor restart
 #
-#
+# pip install -U requests 
+# pip install requests[socks]
 
 from stem import Signal
 from stem.control import Controller
+import requests
+
+def get_current_ip():
+    proxies = {'http':'socks5h://127.0.0.1:9050', 'https':'socks5h://127.0.0.1:9050'}
+    result = requests.get("http://api.ipify.org?format=json",  proxies=proxies)
+    # result = requests.get("http://api.ipify.org?format=json")
+    return result.text
 
 with Controller.from_port(port = 9051) as controller:
+    print(get_current_ip())
     controller.authenticate(password='technik')
-    print("Success!")
+    #print("Success!")
     # print(dir(controller))
     controller.signal(Signal.NEWNYM)
-    print("New Tor connection processed")
+    print(get_current_ip())
