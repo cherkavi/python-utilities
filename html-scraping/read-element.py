8ffc1ab5f479fb28b69c596c1997eebfc520e0df
Vitalii <technik7job@gmail.com>
2018-01-27 19:21:11 +0100

    Virtual screen with Firefox Selenium driver

---
+++ html-scraping/read-element.py
@@ -0,0 +1,17 @@
+from pprint import pprint
+from selenium import webdriver
+from xvfbwrapper import Xvfb
+
+
+vdisplay = Xvfb()
+vdisplay.start()
+response = webdriver.Firefox()
+# url = 'http://www.mctrek.de/bekleidung-unisex-herren/wintersport-skibekleidung/jacken/jack-wolfskin-icy-storm-flex-jacke-m_4047919'
+url = 'http://www.mctrek.de/bekleidung-unisex-herren/wintersport-skibekleidung/jacken/icepeak-kurt-wintersportjacke-herren_4047336'
+response.get(url)
+search_element = response.find_elements_by_xpath('//*[@id="wk_addItem"]/option')
+# pprint(search_element)
+# print(dir(search_element))
+for each in search_element:
+	print(each.get_attribute('innerHTML').strip())
+vdisplay.stop()
