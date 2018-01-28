8136dbf1c318fd9fe3005adac1314b6718178ca2
Vitalii <technik7job@gmail.com>
2018-01-28 21:53:46 +0100

    approach to get values from SelectElement

--- html-scraping/lxml/parse_page.py
+++ html-scraping/lxml/parse_page.py
@@ -1,5 +1,8 @@
 from lxml import html
 
 page = html.parse("http://www.mctrek.de/bekleidung-unisex-herren/wintersport-skibekleidung/jacken/icepeak-kurt-wintersportjacke-herren_4047336")
-list_of_size = page.xpath('//*[@id="wk_addItem"]/option/text()')
-print(list_of_size)
+# list_of_size = page.xpath('//*[@id="wk_addItem"]/option/text()')
+# print(list_of_size)
+
+list_of_size = page.xpath('//*[@id="wk_addItem"]')
+print(list_of_size[0].value_options)
