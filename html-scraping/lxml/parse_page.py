b7a288b32165eb3b3b3fa5e138a5392dcaf3ba3f
Vitalii <technik7job@gmail.com>
2018-01-28 21:45:06 +0100

    using lxml.de lib

---
+++ html-scraping/lxml/parse_page.py
@@ -0,0 +1,5 @@
+from lxml import html
+
+page = html.parse("http://www.mctrek.de/bekleidung-unisex-herren/wintersport-skibekleidung/jacken/icepeak-kurt-wintersportjacke-herren_4047336")
+list_of_size = page.xpath('//*[@id="wk_addItem"]/option/text()')
+print(list_of_size)
