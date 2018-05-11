e90d1039f95d92ebe1bf2a4ddaec2e2a0c0ac385
Vitalii Cherkashyn <technik7job@gmail.com>
2018-05-11 10:11:19 +0200

    pip uninstall

--- pip-manage.py
+++ pip-manage.py
@@ -3,3 +3,4 @@
 import pip
 pip.main(['install', 'stem']) # install package
 # pip.main(['install', '-U', 'requests']) # update package
+# pip.main(['uninstall ', '-y', 'requests']) # uninstall package
