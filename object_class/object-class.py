7fb9edcc69ded87730546996b0c800a366cf5f24
Vitali Cherkashyn <Vitali.Cherkashyn@vodafone.com>
2018-07-16 16:37:37 +0200

    data model, example of __class__ and __new__

--- object_class/object-class.py
+++ object_class/object-class.py
@@ -1,3 +1,5 @@
+# https://docs.python.org/3/reference/datamodel.html?highlight=__add__#special-method-names
+
 class Info:
 	def __init__(self, value):
 		self.value = value
@@ -7,6 +9,9 @@ class Info:
 
 	def new_object(self, new_value):
 		return self.__class__(new_value)
+
+	def new_object2(self, new_value):
+		return self.__new__(new_value)
 
 
 info1 = Info("value1")
@@ -18,3 +23,8 @@ info2 = info1.new_object("value2")
 print(dir(info2))
 print(type(info2))
 print(info2)
+
+info3 = info1.new_object("value3")
+print(dir(info3))
+print(type(info3))
+print(info3)
