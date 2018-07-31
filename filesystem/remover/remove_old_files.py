617d73cecd741ec710a67b296795265840abed07
Vitali Cherkashyn <Vitali.Cherkashyn@vodafone.com>
2018-07-31 13:36:32 +0200

    command line argument

--- filesystem/remover/remove_old_files.py
+++ filesystem/remover/remove_old_files.py
@@ -3,6 +3,8 @@ import sys
 import re
 import datetime
 
+# application to remove files with filtering by name ( regular expression ) and by creation date ( how old )
+
 def action_for_older_than_days(folder, action, matchers):
 	for each_file in os.listdir(folder):
 		matchers_result = [each_matcher(folder, each_file) for each_matcher in matchers]
@@ -11,7 +13,7 @@ def action_for_older_than_days(folder, action, matchers):
 
 
 def remove_file(folder, filename):
-	print(os.path.join(folder, filename))
+	os.unlink(os.path.join(folder, filename))
 
 
 def match_file_by_mask(folder, filename):
@@ -22,9 +24,14 @@ def match_file_by_date(folder, filename):
 	# return datetime.datetime.fromtimestamp(os.stat(os.path.join(folder, filename)).st_ctime) < datetime.datetime.now()-datetime.timedelta(days=180)
 	return datetime.datetime.fromtimestamp(os.stat(os.path.join(folder, filename)).st_ctime) < datetime.datetime.now()-datetime.timedelta(seconds=100)
 
-
+# expected to have command line parameter with name of the folder, otherwise hard-coded value will be applied
 if __name__=="__main__":
-	control_folder = "c:\\temp\\remover\\test\\"
+	control_folder = "/data/wmu/"
+
+	if len(sys.argv)>1:
+		control_folder = sys.argv[1]
+
+	print(control_folder)
 
 	if not os.path.exists(control_folder):
 		print("path is not exists")
