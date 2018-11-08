56fbadc92aa5651a7fc74e95908c36909b2ed2a3
Vitalii Cherkashyn <technik7job@gmail.com>
2018-11-08 10:07:58 +0000

    find file by suffix with options

--- find-file.py
+++ find-file.py
@@ -1,4 +1,11 @@
 import os
+
+hash = "md5"
+suffix = ".py"
+
+hash_options = [hash[-len:]+suffix for len in range(len(hash)+1, 1, -1) ]
+print(hash_options)
 for file in os.listdir("."):
-    if file.endswith(".py"):
-        print(file)
+	for each_option in hash_options:
+		if file.endswith(each_option):
+			print(file)
