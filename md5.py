b2a01d3e34959fc48584a215a7d36958ea4a2b2f
technik <technik7job@gmail.com>
2018-11-11 12:20:31 +0100

    md5 sum for file, md5sum for string

--- md5.py
+++ md5.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
-import md5 as md5hash from md5
 
-print(md5("hello"))
+# python 2.7.x 
+# import md5 as md5hash from md5
+# print(md5("hello"))
+
+# python 3.5.x
+import hashlib
+hashlib.md5(open('/home/technik/projects/temp/ista/json-source/2018110815330826780340100-2018110815330829280340100-956.json','rb').read().encode("utf-8")).hexdigest()
+hashlib.md5(b"hello").hexdigest()
