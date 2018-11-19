eba279007e85c71ff6eaf274af342ab4f4bfaf85
technik <technik7job@gmail.com>
2018-11-19 05:09:23 +0100

    to csv converter from json

--- xml-xpath/json2csv.py
+++ xml-xpath/json2csv.py
@@ -2,13 +2,19 @@ import json
 import logging
 import os
 import sys
+import column_mapper
 
 
 def main(path_to_json, path_to_csv):
     with open(path_to_json) as f:
         json_file = json.load(f)
+    need_column = True
     with open(path_to_csv, "w") as output:
         for each_row in json_file["Data"]:
+            if need_column:
+                need_column = False
+                output.write(",".join([column_mapper.column_by_value(key) for key, value in each_row.items()]))
+                output.write("\n")
             output.write(",".join([str(value) for key, value in each_row.items()]))
             output.write("\n")
 
