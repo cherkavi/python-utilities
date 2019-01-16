be7bf124a78ca71631e267a93360afd1bfa50488
technik <technik7job@gmail.com>
2019-01-16 07:28:44 +0100

    format with specifying output values

--- format-output/format.py
+++ format-output/format.py
@@ -4,4 +4,6 @@ b = 20
 c = "hello"
 print( f"{a} {{escape example}}  {b}\t{c.upper()}" )
 print( "{}   {}\t{}".format(a,b,c) )
+
 print( "{0:5d}   {1:10d}\t{2:10s}".format(a,b,c) )
+print( "{val2:5d}   {val1:10d}\t{val3:10s}".format(val1=a,val2=b,val3=c) )
