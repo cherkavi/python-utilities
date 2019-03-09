da1874eab5a9e8d3fcd848f396fa46bdd6ea5109
Vitalii <technik7job@gmail.com>
2019-03-09 11:29:58 +0100

    cosmetic changes

--- filesystem/create-remove-folder.py
+++ filesystem/create-remove-folder.py
@@ -5,9 +5,15 @@ import sys
 
 output_folder = "test_folder/test1"
 
+# folder check existence
 if os.path.exists(output_folder):
+	# checking is directory, isdirectory ?
+	# if not os.path.isdir(control_folder):
+
+	# remove folder, delete folder
 	shutil.os.remove(output_folder)
 
+# create folder 
 os.makedirs(output_folder)
 
 
