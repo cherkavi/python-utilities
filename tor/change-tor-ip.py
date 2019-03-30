a7da1ad114703b41b47ae020b9f2f757cd7c044c
technik <technik7job@gmail.com>
2019-03-30 13:24:08 +0100

    cosmetic changes

--- tor/change-tor-ip.py
+++ tor/change-tor-ip.py
@@ -1,6 +1,10 @@
 from stem import Signal
 from stem.control import Controller
 
-with Controller.from_port(port = 9050) as controller:
-    # controller.authenticate()
+# control file
+# /etc/tor/torrc
+# generate password
+# tor --hash-password mypassword
+with Controller.from_port(port = 9050) as controller: #ControlPort 9050 
+    controller.authenticate(password="technik")       #HashedControlPassword 16:872860B76453A77D60CA2BB8C1A7042072093276A3D701AD684053EC4C 
     controller.signal(Signal.NEWNYM)
