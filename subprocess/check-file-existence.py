d242ddfb18d7795448af5868b21ede57f83b407e
Vitalii Cherkashyn <technik7job@gmail.com>
2019-04-05 20:53:48 +0200

    suppress output stdout, stderr

--- subprocess/check-file-existence.py
+++ subprocess/check-file-existence.py
@@ -1,6 +1,10 @@
 import subprocess
+import os
+
+FNULL = open(os.devnull, 'w')
+
 try:
-	value = subprocess.check_call(["ls","/data/store/collected/car-data/4f1a-a1b4-4bee05c9c281/MDF4/20181129T105009.MF4"], shell=False, stderr=None, stdout=None)
+	value = subprocess.check_call(["ls","/data/store/collected/car-data/4f1a-a1b4-4bee05c9c281/MDF4/20181129T105009.MF4"], shell=False, stderr=FNULL, stdout=FNULL)
 except Exception as e:
 	print(e.returncode)
 else:
