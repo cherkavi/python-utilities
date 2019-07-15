183be3af0dbc708b0dd0f6f7989b6b78ac1a3370
technik <technik7job@gmail.com>
2019-07-15 06:36:53 +0200

    change to command line arguments reading

--- mail/imap-client.py
+++ mail/imap-client.py
@@ -1,8 +1,9 @@
 # pip install imapclient
+import sys
 from imapclient import IMAPClient
 
-mail_login = ''
-mail_password = ''
+mail_login = sys.argv[1]
+mail_password = sys.argv[2]
 with IMAPClient('imap.ukr.net', port=993, use_uid=True) as server:
 	try:
 		login_result = server.login(mail_login, mail_password)
