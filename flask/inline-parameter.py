05823604115ff5ed102c3271bf273b66dae400f0
Vitalii Cherkashyn <technik7job@gmail.com>
2019-07-22 09:36:13 +0200

    flask return code, post request

--- flask/inline-parameter.py
+++ flask/inline-parameter.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
-
+import os
+import uuid
+import jsonschema
 import subprocess
 
 # example of triggering app
@@ -10,9 +12,24 @@ import subprocess
 app = Flask(__name__)
 # api = Api(app=app)
 
+
 @app.route("/<job_id>", methods=['GET'])
 def get_simulation_job(job_id):
     return jsonify(message="job id is: {}!".format(job_id))
+
+
+@app.route("/simulation", methods=['POST'])
+def create_simulation_job():
+    # post request read data
+    job_request = request.get_json(silent=True)
+    job_id = uuid.uuid1()
+    return job_id
+
+
+@app.route("/scenario-extraction/<uuid:job_id>", methods=['GET'])
+def get_scenario_extraction_job(job_id):
+    # return code 
+    return jsonify(message="some message %s" % (str(job_id))), 404
 
 
 if __name__ == "__main__":
