ee38ba0dbd382dedd29ad03d3f4ed8d8626c6347
technik <technik7job@gmail.com>
2019-07-28 17:59:44 +0200

    close 'browser', delete all cookies

--- html-scraping/selenium/read-element.py
+++ html-scraping/selenium/read-element.py
@@ -10,13 +10,17 @@ from pprint import pprint
 vdisplay = Xvfb()
 # vdisplay = Display(visible=0)
 vdisplay.start()
-response = webdriver.Firefox()
+driver = webdriver.Firefox()
 # url = 'http://www.mctrek.de/bekleidung-unisex-herren/wintersport-skibekleidung/jacken/jack-wolfskin-icy-storm-flex-jacke-m_4047919'
 url = 'http://www.mctrek.de/bekleidung-unisex-herren/wintersport-skibekleidung/jacken/icepeak-kurt-wintersportjacke-herren_4047336'
-response.get(url)
-search_element = response.find_elements_by_xpath('//*[@id="wk_addItem"]/option')
+driver.get(url)
+search_element = driver.find_elements_by_xpath('//*[@id="wk_addItem"]/option')
 # pprint(search_element)
 # print(dir(search_element))
 for each in search_element:
 	print(each.get_attribute('innerHTML').strip())
 vdisplay.stop()
+
+driver.delete_all_cookies()
+# driver.refresh()
+driver.close()
