2e9146701701adc0276524b85f0b9915eb979496
Vitalii Cherkashyn <technik7job@gmail.com>
2019-08-02 11:09:26 +0200

    shutdown signal listener

--- os-package.py
+++ os-package.py
@@ -11,6 +11,22 @@ import time
 # send signal to current process
 signal.signal(signal.SIGHUP, signal.SIG_IGN)
 
+
+class SignalListener:
+    """
+    listen shutdown signal from Operation System
+    """
+    def __init__(self):
+        self.shutdown = False
+        signal.signal(signal.SIGINT, self._process_shutdown)
+        signal.signal(signal.SIGTERM, self._process_shutdown)
+
+    def _process_shutdown(self, signum, frame):
+        self.shutdown = True
+        logger = logging.getLogger(self.__class__.__name__)
+        logger.info("shutdown !!! ")
+        
+
 # pid id of current process
 os.getpid()
 
