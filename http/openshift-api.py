42cb8e17f567c38669d350eb66371633e2729e1a
Vitalii Cherkashyn <technik7job@gmail.com>
2019-08-05 10:22:52 +0200

    udpate with create/delete resources

--- http/openshift-api.py
+++ http/openshift-api.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import requests
+import json
+import yaml
 
 
 def clear_binary_line(b_line):
@@ -8,6 +10,47 @@ def clear_binary_line(b_line):
     if next_line[len(next_line)-1] == "\n":
         next_line = next_line[:-1]
     return next_line
+
+
+def get_all_pods(endpoint, namespace, token):
+    url = f"{endpoint}/api/v1/namespaces/{namespace}/pods?limit=10&fieldSelector=status.phase%3DRunning"
+    response = requests.get(url,
+                            verify=False,
+                            headers={"Authorization": f"Bearer {token}", "Accept": "application/json"})
+    return [each_item['metadata']['name'] for each_item in response.json()['items']]
+    # print(response.json()['metadata']['selfLink'])
+
+
+def create(endpoint, namespace, token, path_to_file):
+    url = f"{endpoint}/api/v1/namespaces/{namespace}/pods"
+    with open(path_to_file, 'r') as data_file:
+        yaml_data = yaml.load(data_file)
+    data = json.dumps(yaml_data)
+    response = requests.post(url,
+                             verify=False,
+                             headers={"Authorization": f"Bearer {token}",
+                                      "Accept": "application/json",
+                                      "Content-Type": "application/json"
+                                      },
+                             data=data)
+    return response.json()
+
+
+def delete(endpoint, namespace, token, path_to_file):
+    with open(path_to_file, 'r') as data_file:
+        yaml_data = yaml.load(data_file)
+    resource_type = yaml_data['kind'].lower()
+    resource_name = yaml_data['metadata']['name']
+    url = f"{endpoint}/api/v1/namespaces/{namespace}/{resource_type}s/{resource_name}"
+    response = requests.delete(url,
+                               verify=False,
+                               headers={"Authorization": f"Bearer {token}",
+                                        "Accept": "application/json",
+                                        "Content-Type": "application/json"
+                                        }
+                               )
+    return response.json
+
 
 def main():
     token = clear_binary_line(subprocess.check_output(["oc", "whoami", "-t"]))
@@ -18,10 +61,13 @@ def main():
     status = clear_binary_line(subprocess.check_output(["oc", "status"])).split("\n")[0].split(" ")
     endpoint = status[5]
     namespace = status[2]
-    url = f"{endpoint}/api/v1/namespaces/{namespace}/pods?limit=10&fieldSelector=status.phase%3DRunning"
-    response = requests.get(url, verify=False, headers={"Authorization": f"Bearer {token}", "Accept": "application/json"})
-    print(response.content)
-    print(response.json()['metadata']['selfLink'])
+
+    # print(get_all_pods(endpoint, namespace, token))
+    print("delete")
+    print(delete(endpoint, namespace, token, "/home/projects/current-task/openshift-docker/working-example-pod.yml"))
+    print("create")
+    print(create(endpoint, namespace, token, "/home/projects/current-task/openshift-docker/working-example-pod.yml"))
+
 
 if __name__ == '__main__':
     main()
