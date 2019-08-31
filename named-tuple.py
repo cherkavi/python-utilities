949f32a8b4c4eeeac085fcb01ef460983a7793f4
technik <technik7job@gmail.com>
2019-09-01 00:08:07 +0200

    namedtuple class name

--- named-tuple.py
+++ named-tuple.py
@@ -3,7 +3,8 @@ from collections import namedtuple
 CustomComputer = namedtuple("Computer", ["keyboard", "mouse", "monitor"])
 my_computer = CustomComputer("Apple", "Logitech", ["Dell", "Dell", "NEC", "Built-in"])
 
-print(my_computer) 
+# pay attention, namedtuple - CustomComputer, but class - Computer
+print(my_computer) # Computer(keyboard='Apple', mouse='Logitech', monitor=['Dell', 'Dell', 'NEC', 'Built-in'])
 # unpack parameters
 print(*my_computer)    # Apple Logitech ['Dell', 'Dell', 'NEC', 'Built-in']
 (my_keyboard, my_mouse, my_monitor) = my_computer
@@ -16,3 +17,8 @@ print(my_computer[2])  # ["Dell", "Dell", "NEC", "Built-in"]
 print(my_computer.keyboard) # Apple
 print(my_computer.mouse)    # Logitech
 print(my_computer.monitor)  # ["Dell", "Dell", "NEC", "Built-in"]
+
+
+# extension
+CustomNotebook = namedtuple("Notebook", CustomComputer._fields + ('battery', ))
+print(CustomNotebook)
