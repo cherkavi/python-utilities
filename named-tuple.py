ed46426b58746ed3dd8cc23cb278525c9a24bd1e
technik <technik7job@gmail.com>
2019-09-01 00:01:50 +0200

    unpack variables

--- named-tuple.py
+++ named-tuple.py
@@ -4,9 +4,15 @@ CustomComputer = namedtuple("Computer", ["keyboard", "mouse", "monitor"])
 my_computer = CustomComputer("Apple", "Logitech", ["Dell", "Dell", "NEC", "Built-in"])
 
 print(my_computer) 
+# unpack parameters
+print(*my_computer)    # Apple Logitech ['Dell', 'Dell', 'NEC', 'Built-in']
+(my_keyboard, my_mouse, my_monitor) = my_computer
+print(my_keyboard, my_mouse, my_monitor) # Apple Logitech ['Dell', 'Dell', 'NEC', 'Built-in']
+# print fields by index
 print(my_computer[0])  # Apple
 print(my_computer[1])  # Logitech
 print(my_computer[2])  # ["Dell", "Dell", "NEC", "Built-in"]
+# print fields by name 
 print(my_computer.keyboard) # Apple
 print(my_computer.mouse)    # Logitech
 print(my_computer.monitor)  # ["Dell", "Dell", "NEC", "Built-in"]
