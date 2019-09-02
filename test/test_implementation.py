36c5cbaadde64c88147712fc0ff04e4c5109b455
technik <technik7job@gmail.com>
2019-09-02 06:31:30 +0200

    pytest setup/teardown

--- test/test_implementation.py
+++ test/test_implementation.py
@@ -1,10 +1,19 @@
+import pytest
 from implementation import summarize_digits
+
 # for execution just run
 # pytest .
 
-def setup():
-    print("setup")
+@pytest.fixture(scope='function')
+def some_resource(request):
+    stuff_i_setup = ["I setup"]
 
+    def some_teardown():
+        stuff_i_setup[0] += " ... but now I'm torn down..."
+        print(stuff_i_setup[0])
+    request.addfinalizer(some_teardown)
+
+    return stuff_i_setup[0]
 def test_one():
     # given
     a=5
