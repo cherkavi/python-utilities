f2b603f0c24140073ccfe43dc290c043df1259ec
Vitalii Cherkashyn <technik7job@gmail.com>
2019-09-09 11:00:12 +0200

    import remote file, import not installed package, dynamic import

---
+++ remote-import.py
@@ -0,0 +1,11 @@
+# import file/module not in your "classpath"
+# import remote file from classpath
+
+# approach 1
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/path/to/application/app/folder')
+import destination_file
+
+# approach 2
+# from full.path.to.python.folder import func_name
