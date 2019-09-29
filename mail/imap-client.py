4744d0dd394d342838700a7fb695aa5e82440751
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2019-09-29 22:06:41 +0200

    cleanup code

--- mail/imap-client.py
+++ mail/imap-client.py
@@ -4,17 +4,28 @@ from imapclient import IMAPClient
 
 mail_login = sys.argv[1]
 mail_password = sys.argv[2]
-with IMAPClient('imap.ukr.net', port=993, use_uid=True) as server:
+
+mail_server = 'imap.ukr.net'
+mail_port = 993
+
+def process_message(text):
+        lines=list(filter(lambda each_line: each_line.find("xing.com/m")>=0, text.split("\\r\\n")))
+        if len(lines)>0:
+                return lines[1]
+        else:
+                return None
+
+
+with IMAPClient(mail_server, port=mail_port, use_uid=True) as server:
         try:
                 login_result = server.login(mail_login, mail_password)
                 inboxInfo = server.select_folder('INBOX')
                 messages = server.search(['NOT', 'DELETED'])
                 response = server.fetch( messages, ['RFC822', 'BODY[TEXT]'] )
 
-                for msgid, data in response.items():                        
-                        lines=list(filter(lambda each_line: each_line.find("View message in browser:")>=0, str(data[b'BODY[TEXT]']).split("\\r\\n")))
-                        if len(lines)>0:
-                                print(lines[0].split(": ")[1].strip())
+                for msgid, data in response.items():
+                        text = str(data[b'BODY[TEXT]'])
+                        print(process_message(text))
                         print("---")
         except Exception as e:
                 print("error:", e.args[0])
