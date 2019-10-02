a6dbccb8615e74302fa606058663b3aec9a1e593
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2019-10-02 22:00:18 +0200

    read keys from keyboard, blocking keytype

---
+++ keypress-read.py
@@ -0,0 +1,20 @@
+# keyboard management: 
+# * input: getch
+# * output: click
+# windows: msvcrt
+
+import getch
+
+def waiting_for_key()->[]:
+    """
+    blocking read from keyboard
+    get char from keyboard
+    get keytype
+    """
+    read_symbol_ord = ord(getch.getch())
+    if read_symbol_ord == 27:
+        return [read_symbol_ord, ord(getch.getch()), ord(getch.getch())]
+    else:
+        return [read_symbol_ord, ]
+
+print(waiting_for_key())
