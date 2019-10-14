09253dde83dc1c8f3e7126983aec746e6edb74a5
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2019-10-14 09:45:56 +0200

    thread based on function, multiprocessing Process

--- thread/function-in-thread.py
+++ thread/function-in-thread.py
@@ -7,6 +7,7 @@ def long_time_processing(prefix:str, amount_of_iteration:int):
         sleep(0.3)
 
 if __name__=="__main__":
+    # start Thread from function, function thread
     Process(target=long_time_processing, args=("first ", 8)).start()
     sleep(1)
     Process(target=long_time_processing, args=("second ", 5)).start()
