da20ee613008896c70f7bad156b3893d4987998a
Vitalii Cherkashyn <technik7job@gmail.com>
2019-11-26 18:52:22 +0100

    get error message from exception

--- exceptions/read_file.py
+++ exceptions/read_file.py
@@ -1,7 +1,11 @@
 try:
     f = open('readme.md')
 except IOError as e:
-    print( 'can't open the file: %s' % (e.args[0], ) )
+    if isinstance(e, Iterable) and len(e) > 0:
+        error_message = e[0].message
+    else:
+        error_message = e.message
+    print( 'can't open the file: %s' % (error_message, ) )
 else:
     with f:
         print f.readlines()
