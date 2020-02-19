b3a3d4d3792ae87fc55a935307206dbfd34ffd35
Vitalii Cherkashyn <technik7job@gmail.com>
2020-02-19 06:57:08 +0100

    multiply parameters

--- input-parameters/input_args.py
+++ input-parameters/input_args.py
@@ -11,6 +11,11 @@ if __name__ == "__main__":
     parser.add_argument('--destination_user',
                         help='user of DB where script should be applied ',
                         required=False, default="default_schema")
+    
+    # list of parameters like: --list 101 102 103 104
+    parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', required=True)
+    # you can use nargs='*' in case of 0..* parameters instead of nargs='+' that means 1..*
+    
     args = parser.parse_args()
 
     main(args.version1, args.version2, args.recreate_db_user, args.create_user_sql, args.destination_user, True)
