18b5afc08a87ede420361c9c47a91323164387ab
Vitalii Cherkashyn <technik7job@gmail.com>
2020-04-18 21:57:12 +0200

    additional tag

--- subprocess/execute-batch-job.py
+++ subprocess/execute-batch-job.py
@@ -20,6 +20,7 @@ black_list = ["C:/project/horus/horus-batch-deploy/target/horus-batch-deploy-8.2
 
 
 def clear_binary_line(b_line):
+    # convert bytes to string, bytes2string, byte to string
     next_line = b_line.decode('utf-8')
     if next_line[len(next_line)-1] == "\n":
         next_line = next_line[:-1]
