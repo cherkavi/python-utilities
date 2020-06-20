4c6e4e531d699459715b65722cab5a8fb37563de
Vitalii Cherkashyn <technik7job@gmail.com>
2020-06-20 18:33:01 +0200

    decorator examples

--- decorator/custom-decorator.py
+++ decorator/custom-decorator.py
@@ -1,17 +1,63 @@
-def my_custom_wrapper(target_function, *args, **kwargs):
-    def my_wrapper(*args, **kwargs):
-        print("arguments: " + str(args))
-        return_value = target_function(*args, **kwargs)
-        print("return value: "+str(return_value))
+# ----------------------------------------
+# function without parameters
+def my_simple_decorator(function):
+    def simple_decorator():
+        return_value=function()        
+        print(f"simple wrapper: {return_value}")
         return return_value
-    return my_wrapper
+    return simple_decorator
 
-@my_custom_wrapper
-def sum(a=10, b=None):
-    if not b:
-        b = a
+@my_simple_decorator
+def summarize2()->int:
+    return 10
+
+
+# ----------------------------------------
+# with function parameters
+def my_decorator(target_function):
+    def decorator(*args, **kwargs):
+        return_value=target_function(*args, **kwargs)        
+        print(f"wrapper: {return_value}")
+        return return_value
+    return decorator
+
+@my_decorator
+def summarize(a:int, b:int)->int:
     return a+b
+
+# ----------------------------------------
+# function without parameters, decorator with parameters 
+def my_another_decorator(*args, **kwargs):
+    def another_decorator(func):
+        print(kwargs['description'])
+        return func
+    return another_decorator
+
+@my_another_decorator(description="another description: ")
+def summarize_another()->int:
+    return 20
+
+# ----------------------------------------
+# function with parameters, decorator with parameters 
+def my_complex_decorator(description:str):
+    def complex_decorator(func):
+        def complex_decorator_wrapper(*args, **kwargs):
+            return_value = func(*args, **kwargs)
+            print(f"{description}:  {return_value}")
+            return return_value
+        return complex_decorator_wrapper
+    return complex_decorator
+
+@my_complex_decorator(description="my own description: ")
+def summarize_complex(a:int)->int:
+    return a+a
 
 
 if __name__=='__main__':
-    print(sum(10,20))
+    print(f"main.summarize2: {summarize2()}")
+    print("------------")
+    print(f"main.summarize: {summarize(30,40)}")
+    print("------------")
+    print(f"main.summarize: {summarize_another()}")
+    print("------------")
+    print(f"main.summarize_complex: {summarize_complex(20)}")
