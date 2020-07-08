e41699335d488bf717baf76b2d7e3f8bde0aa19e
Vitalii Cherkashyn <technik7job@gmail.com>
2020-07-08 18:39:31 +0200

    example of debugging requests lib

--- http/request-debug.py
+++ http/request-debug.py
@@ -1,6 +1,10 @@
+code="bwvdgfa"
+
+
 import requests
 import logging
 
+# example of importing, import error
 try:
     import http.client as http_client
 except ImportError:
@@ -15,4 +19,13 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-requests.get('https://api.login.yahoo.com/oauth2')
+headers={"Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded"}
+# Accept: */*
+# Content-Type: application/x-www-form-urlencoded
+
+client_id="dj0yJmk"
+client_secret="a871c"
+redirect_uri="https://e-176-43.eu-central-1.compute.amazonaws.com"
+data=f"code={code}&grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&response_type=code"
+url="https://api.login.yahoo.com/oauth2/get_token"
+print(requests.post(url, data=data, headers=headers))
---
+++ subprocess/curl.py
@@ -0,0 +1,31 @@
+import subprocess
+import json
+from typing import List
+
+def clear_binary_line(b_line:bytes) -> str:
+    # convert bytes to string, bytes2string, byte to string
+    next_line = b_line.decode('utf-8')
+    if next_line[len(next_line)-1] == "\n":
+        next_line = next_line[:-1]
+    index = next_line.rfind("/")
+    if index >= 0:
+        next_line = next_line[index+1:]
+    return next_line
+
+# subprocess curl, requests emulator, requests clear call
+code="bx4pqba"
+client_id="dj0yJmTBj"
+client_secret="a871c009"
+redirect_uri="https://ec2-5.eu-central-1.compute.amazonaws.com"
+command=f"curl -X POST https://api.login.yahoo.com/oauth2/get_token --data 'code={code}&grant_type=authorization_code&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&response_type=code'"
+output:bytes = subprocess.check_output(command.split(" "))
+response: dict = json.loads(clear_binary_line(output))
+
+if "error" in response:
+    print(response["error"])
+    print(response["error_description"])
+else:
+    print(response["access_token"])
+    print(response["refresh_token"])
+    print(response["expires_in"])
+    print(response["bearer"])
