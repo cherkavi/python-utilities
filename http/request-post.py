d67012fc0d4cb66f489824efeacd49c981058c0b
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2020-10-30 22:36:04 +0100

    requests post with fields

---
+++ http/request-post.py
@@ -0,0 +1,13 @@
+import logging
+
+import requests
+
+_log = logging.getLogger(__name__)
+
+def _check_captcha_via_rest(secret: str, response: str) -> bool:
+    fields_to_send = {
+        "secret": secret,
+        "response": response}
+    # response = requests.post("https://www.google.com/recaptcha/api/siteverify", data=fields_to_send, files={os.path.basename(sample): open(sample, "rb")})
+    response_raw = requests.post("https://www.google.com/recaptcha/api/siteverify", data=fields_to_send)
+
