825fc85f43275f6d1e2c2043abd5ffde306c6a20
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2020-10-30 22:52:43 +0100

    example of controlling request

---
+++ http/request-grain-control.py
@@ -0,0 +1,17 @@
+import requests
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+from requests import RequestException
+
+s = requests.Session()
+
+retries = Retry(total=5,
+                backoff_factor=0.1,
+                status_forcelist=[ 500, 502, 503, 504 ])
+
+s.mount('http://', HTTPAdapter(max_retries=retries))
+
+try:
+    s.get('http://httpstat.us/500')
+except RequestException as ex:
+    print(ex)
