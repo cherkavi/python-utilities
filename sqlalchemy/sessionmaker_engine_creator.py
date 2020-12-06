c673a4661e3316cf429f84b8e89a8d6e26a5b315
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2020-12-06 22:27:12 +0100

    sqlite with disk persistence

--- sqlalchemy/sessionmaker_engine_creator.py
+++ sqlalchemy/sessionmaker_engine_creator.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 def get_engine_and_session_cls(echo=True):
+    # engine = create_engine(f"sqlite:///{db_path}?cache=shared", echo=False, connect_args={'check_same_thread': False})
     engine = create_engine("sqlite:///:memory:", echo=echo)
     session_class = sessionmaker(bind=engine)
     return engine, session_class
