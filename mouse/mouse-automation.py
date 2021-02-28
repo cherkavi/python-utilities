8390a8ca9b4515745bbbf8486bce7d613e4d270c
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2021-02-28 20:59:13 +0100

    mouse automation

---
+++ mouse/mouse-automation.py
@@ -0,0 +1,24 @@
+# Xlib.error.DisplayConnectionError: Can't connect to display ":1": b'No protocol specified\n'
+# temp solution: xhost +
+
+
+import keyboard
+import time
+import pyautogui
+
+if __name__ == '__main__':
+
+    ### record all input keys
+    # recorded: [] = keyboard.record(until='esc')
+    # print(recorded)
+
+
+    keyboard.add_hotkey('alt+p', lambda : pyautogui.moveRel(100,100))
+    ### waiting for ctrl-c, control c
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        pass
+
+    keyboard.clear_hotkey('alt+p')
