33ea8e97a2839efcedac01491e702f9bcc8c0a1d
Vitalii Cherkashyn <vitalii.cherkashyn@heritageweb.com>
2021-03-27 23:29:46 +0100

    send json request

--- http/request-post.py
+++ http/request-post.py
@@ -11,6 +11,8 @@ def _check_captcha_via_rest(secret: str, response: str) -> bool:
     # response = requests.post("https://www.google.com/recaptcha/api/siteverify", data=fields_to_send, files={os.path.basename(sample): open(sample, "rb")})
 
     response_raw = requests.post("https://www.google.com/recaptcha/api/siteverify", data=fields_to_send)
-    # !!! in some cases !!!:
-    # data = json.dumps(fields_to_send)
+    # !!! it will send object as a url encoded string: secret=my_secret&response=99999
+
+    # if you want to send in JSON, requests json, request json
+    response_raw = requests.post("https://www.google.com/recaptcha/api/siteverify", data=json.dumps(fields_to_send))
 
