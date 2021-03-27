0b507c3a40d4b9308d7a7396511e245d8fd56fd0
Vitalii Cherkashyn <vitalii.cherkashyn@heritageweb.com>
2021-03-27 23:23:25 +0100

    some API has bad singularity

--- http/request-post.py
+++ http/request-post.py
@@ -9,5 +9,8 @@ def _check_captcha_via_rest(secret: str, response: str) -> bool:
         "secret": secret,
         "response": response}
     # response = requests.post("https://www.google.com/recaptcha/api/siteverify", data=fields_to_send, files={os.path.basename(sample): open(sample, "rb")})
+
     response_raw = requests.post("https://www.google.com/recaptcha/api/siteverify", data=fields_to_send)
+    # !!! in some cases !!!:
+    # data = json.dumps(fields_to_send)
 
