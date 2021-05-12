f7f49f78bfcc3a6e64f14e892e8c52cc8c3c0ecd
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2021-05-12 13:54:29 +0200

    get request headers

--- http/request-get.py
+++ http/request-get.py
@@ -1,3 +1,3 @@
 import requests
-response: requests.Response = requests.get(url, cookies={"PHPSESSID": php_session_id })`
+response: requests.Response = requests.get(url, cookies={"PHPSESSID": php_session_id }, headers={"x-api-key": self._api_key})`
 return response.json().get("user_id", None)
