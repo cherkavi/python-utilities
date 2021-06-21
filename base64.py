40b27889fefeda9161799b971eea7c5219b427d1
Vitalii Cherkashyn <vcherkashyn@wondersign.com>
2021-06-21 23:51:31 +0200

    base64, download binary

--- base64.py
+++ base64.py
@@ -3,3 +3,7 @@ import base64
 print(base64.encodestring('login:password'.encode()))
 print(base64.encodestring('weblogic:weblogic1'.encode()))
 
+
+#               with open(path_to_image_file, "rb") as f:
+#                   encoded = base64.b64encode(f.read())
+
