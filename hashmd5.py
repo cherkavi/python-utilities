99ec84f8191349217aeb6d644e106a53d455bcc3
Vitalii Cherkashyn <vcherkashyn@wondersign.com>
2021-10-24 22:58:35 +0200

    md5sum from bash script calling

--- hashmd5.py
+++ hashmd5.py
@@ -1,3 +1,14 @@
 import sys
 import hashlib
-print(hashlib.md5(f"Ashley Furniture{sys.argv[1]}".encode("utf-8")).hexdigest())
+from typing import List
+
+parameters:List[str] = list(filter(lambda x: len(x.strip())>0,  sys.argv[1:]))
+
+if len(parameters)==1:
+    print(hashlib.md5(f"Ashley Furniture{parameters[0]}".encode("utf-8")).hexdigest())
+    exit(0)
+
+if len(parameters)==2:
+    print(hashlib.md5(f"{parameters[0]}{parameters[1]}".encode("utf-8")).hexdigest())
+    exit(0)
+
