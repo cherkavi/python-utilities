b1a9a39b0b9bae28a102aeb42451c122ab8a69ad
Vitalii Cherkashyn <vitalii.cherkashyn@bmw.de>
2021-11-24 12:45:12 +0100

    * create with __new__ * parameter in method - create new stack reference

--- object_class/object-class.py
+++ object_class/object-class.py
@@ -1,20 +1,23 @@
 # https://docs.python.org/3/reference/datamodel.html?highlight=__add__#special-method-names
 
 class Info:
-	def __init__(self, value):
-		self.value = value
+    def __init__(self, value):
+        self.value = value
 
-	def __str__(self):
-		return self.value
+    def __str__(self):
+        return self.value
 
-	def __repr__(self):
-		return "init value is: {}".format(self.value)
+    def replace_object(self, value:str) -> None:
+        value = "new value"
 
-	def new_object(self, new_value):
-		return self.__class__(new_value)
+    def __repr__(self):
+        return "init value is: {}".format(self.value)
 
-	def new_object2(self, new_value):
-		return self.__new__(new_value)
+    def new_object_with_class(self, new_value):
+        return self.__class__(new_value)
+
+    def new_object_with_new(self, new_value):
+        return self.__new__(new_value)
 
 
 info1 = Info("value1")
@@ -22,15 +25,22 @@ print(dir(info1))
 print(type(info1))
 print(info1)
 print(repr(info1))
+print("--------------")
 
-info2 = info1.new_object("value2")
+info2 = info1.new_object_with_class("value2")
 print(dir(info2))
 print(type(info2))
 print(info2)
 print(repr(info2))
+print("--------------")
 
-info3 = info1.new_object("value3")
+info3 = info1.new_object_with_new(Info)
 print(dir(info3))
 print(type(info3))
-print(info3)
-print(repr(info3))
+#print(info3)
+#print(repr(info3))
+print("--------------")
+
+a = "my original string"
+info1.replace_object(a)
+print(a)
