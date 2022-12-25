5d51766cc3217e4c2e1c21b49c272411e7f92ab3
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2022-12-25 13:08:45 +0100

    redis CRUD

--- redis/connect.py
+++ redis/connect.py
@@ -5,4 +5,8 @@ redis = Redis(host=os.environ.get("REDIS_HOST", "localhost"),
               port=os.environ.get("REDIS_PORT", 6379),
               db=0)
 
+# for entry in redis.hgetall("table_1").values():
+# redis.hset(name="table_1",key="key_1",value="value_1")
+# redis.hdel("table_1", "key_1")
+
 print(redis.get("hello"))
