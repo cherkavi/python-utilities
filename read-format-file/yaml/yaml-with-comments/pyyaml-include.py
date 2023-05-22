0228fa4a38207fe26f253416659831a649ed3342
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2023-05-22 10:41:32 +0200

    read yaml with comments - another attempt

---
+++ read-format-file/yaml/yaml-with-comments/pyyaml-include.py
@@ -0,0 +1,16 @@
+# pip install PyYAML pyyaml-include
+# 
+from yamlinclude import YamlIncludeConstructor
+
+def read_yaml_with_comments(file_path):
+    YamlIncludeConstructor.add_to_loader_class(loader_class=yaml.Loader)
+
+    with open(file_path, 'r') as file:
+        data = yaml.load(file, Loader=Loader)
+
+    return data
+
+if __name__=='__main__':
+    file_path = 'test-data-01.yaml'
+    yaml_data = read_yaml_with_comments(file_path)
+    print(yaml_data)
