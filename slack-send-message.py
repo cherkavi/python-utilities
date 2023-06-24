1e5e0820d7376bed56a085b9aa18b035c1e36a6e
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2023-06-24 09:00:38 +0200

    slack description of prerequisites for application

--- slack-send-message.py
+++ slack-send-message.py
@@ -1,3 +1,14 @@
+### send message to slack channel
+## two env variables are expected:
+## * SLACK_DEFAULT_CHANNEL - name of the channel (#) or id
+## * SLACK_ACCESS_TOKEN - OAuth token of application, [how to](https://github.com/CircleCI-Public/slack-orb/wiki/Setup):
+##   1. create new slack application
+##   2. add rights to application: chat:write, chat:write.public, files:write
+##   3. obtain oauth token, install/add to workspace ( select workspace where app should be )
+##   4. check application in slack, add application as a member to one of the channel
+
+
+## need to install slack library:
 # pip3 install slack_sdk
 import os
 from slack_sdk import WebClient
