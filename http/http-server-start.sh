b41bd1342ab33d8b1f2f4a7bbbca6af48817a5c3
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2023-06-30 10:57:47 +0200

    start with cgi-bin folder for script processing

--- http/http-server-start.sh
+++ http/http-server-start.sh
@@ -1,6 +1,13 @@
+# simple http server with cgi ( script execution ) ability
+
+# https://realpython.com/python-http-server/
+
 # start simple http web server
 python3 -m http.server 9090
 x-www-broser localhost:9090
 
 # or start with more customized version
 nohup python3 http-server.py &
+
+# start with /cgi-bin folder processing
+python3 -m http.server --cgi 9090
