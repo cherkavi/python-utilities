4bddf5e2b8facc65bc23d64b22a908367bc61dcd
Vitalii Ch <vitalii.cherkashyn@gmail.com>
2023-07-22 09:35:11 +0200

    with ignored element in comparison

--- json/compare-json.py
+++ json/compare-json.py
@@ -31,9 +31,25 @@ if __name__ == '__main__':
         data_1 = json.load(file)
     with open(sys.argv[2], 'r') as file:
         data_2 = json.load(file)
-    if compare(data_1, data_2, callback=filter_output):
+    if compare(data_1, data_2, callback=filter_output): # compare(data_1, data_2, callback=filter_output, ignore_path=[r"^(/\d+/customText)"])
         print("ok")
         exit(0)
     else:
         print("ko")
         exit(1)
+
+
+# [
+#     {
+#         "bdcTimestamp": 7828243454778585,
+#         "customText": "DefaultCustomText ignored element in comparasion",
+#         "loggerTimestamp": 1668527613355311616,
+#         "name": "DOE1_START"
+#     },
+#     {
+#         "bdcTimestamp": 7828245980872414,
+#         "customText": "DefaultCustomText",
+#         "loggerTimestamp": 1668527615881492736,
+#         "name": "A1_START"
+#     }
+# ]
