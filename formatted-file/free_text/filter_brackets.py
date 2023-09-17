14555015170ef1dc1e41a9cdf18f98ae6eb2883e
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2023-09-17 07:37:28 +0200

    free text filter app

--- formatted-file/free_text/filter_brackets.py
+++ formatted-file/free_text/filter_brackets.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import re
+import sys
 from typing import List, Set, Union
 
 
@@ -13,10 +14,14 @@ class TextChunk:
 
     def get_text(self, markers_for_filtering: Set[str]) -> Union[str, None]:
         """
-        :param markers_for_filtering: list of markers, that should be checked 
+        :param markers_for_filtering: list of markers, that should be checked
         :return: text or nothing
         """
-        if len(self.markers) and markers_for_filtering and len(markers_for_filtering) > 0:
+        if (
+            len(self.markers)
+            and markers_for_filtering
+            and len(markers_for_filtering) > 0
+        ):
             if len(self.markers.intersection(markers_for_filtering)) > 0:
                 return self.text
             else:
@@ -63,14 +68,14 @@ def get_chunks_from_line(line: str, markers_in_line: Set[str]) -> List[TextChunk
                 next_chunk = TextChunk("")
                 next_chunk.add_marker(nearest_marker)
                 result.append(next_chunk)
-            current_line = current_line[len(nearest_marker):]
+            current_line = current_line[len(nearest_marker) :]
             continue
         next_chunk = TextChunk(current_line[:nearest_marker_position])
         if nearest_marker != "[]":
             # avoid to have empty marker in the set
             next_chunk.add_marker(nearest_marker)
         result.append(next_chunk)
-        current_line = current_line[nearest_marker_position + len(nearest_marker):]
+        current_line = current_line[nearest_marker_position + len(nearest_marker) :]
     return result
 
 
@@ -79,7 +84,7 @@ def get_markers(input_string: str) -> Set[str]:
     :param input_string: string for searching for markers like [ab] [abdsa01] ...
     :return: markers like "[ab]" in the set
     """
-    pattern = r'\[([^]]*)\]'
+    pattern = r"\[([^]]*)\]"
     # pattern = r'\[[^\]]+\](?=(?:\s*\.?$)|\s*$)'
     markers: List[str] = re.findall(pattern, input_string)
     return set([f"[{each_marker}]" for each_marker in markers])
@@ -109,3 +114,21 @@ def filter_lines(lines: List[str], white_markers: Set[str]) -> List[str]:
             # no markers in line
             result.append(each_line)
     return result
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(" <file template src> <file dest> <marker1> <marker2> <marker3> ... ")
+        sys.exit(1)
+    file_source: str = sys.argv[1]
+    print(f"src: {file_source}")
+    file_dest: str = sys.argv[2]
+    print(f"dst: {file_dest}")
+    markers: Set[str] = set()
+    for each_marker in range(3, len(sys.argv)):
+        markers.add(sys.argv[each_marker])
+    print(markers)
+    with open(file_source) as file_src:
+        data = file_src.readlines()
+    with open(file_dest, "w") as file_dst:
+        file_dst.write("\n".join(filter_lines(data, markers)))
