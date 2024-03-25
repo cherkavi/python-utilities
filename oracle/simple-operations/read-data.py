72fa399f9b4efbd27b29a561a988e8fa78417a4f
Vitali <vitalii.cherkashyn@gmail.com>
2024-03-25 17:14:36 +0100

    oracle lib install

--- oracle/simple-operations/read-data.py
+++ oracle/simple-operations/read-data.py
@@ -1,5 +1,7 @@
 # http://www.oracle.com/technetwork/articles/dsl/python-091105.html
+# pip3 install credentials
 import credentials
+# pip3 install cx_oracle
 import cx_Oracle
 
 with cx_Oracle.connect(credentials.db_url) as connection:
