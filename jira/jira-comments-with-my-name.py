aa6f383aadf4a90696761f4e6e316d6395e6cf7b
Vitali <vitalii.cherkashyn@gmail.com>
2024-05-22 11:47:34 +0200

    jira collaboration via REST API

--- jira/jira-comments-with-my-name.py
+++ jira/jira-comments-with-my-name.py
@@ -1,8 +1,17 @@
 from jira import JIRA
+from typing import List, Dict
+from jira.resources import (Comment, Issue)
 import os
+import sys
+import datetime
+
+STRICT: bool = "only-my-comments" in sys.argv[1:]
+TODAY: bool = "only-today" in sys.argv[1:]
+PROJECT_NAME = "EXTRACT"
+USER_NAME = "my-number-245"
 
 def remove_before_minus(s):
-    return "jira-"+s.split('-')[1]
+    return "jira "+s.split('-')[1]+" # "
 
 def add_prefix_to_each_line(s: str):
     prefix = "   "
@@ -12,13 +21,32 @@ def add_prefix_to_each_line(s: str):
 jira = JIRA(server=os.getenv('JIRA_URL'), 
             token_auth=os.getenv('JIRA_TOKEN'))
 
-# ticket = jira.issue('IOO-6728')
-for ticket in jira.search_issues('project = IVSOO AND status not in (Closed) AND text ~ "user-33" order by updated DESC'):
+class Result:
+    def __init__(self, issue: Issue, comments: List[Comment]):
+        self.issue = issue
+        self.comments = comments
+
+results: List[Result] = []
+
+for ticket in jira.search_issues(f'project = {PROJECT_NAME} AND resolution = Unresolved AND text ~ {USER_NAME} order by updated DESC'):
+    comments = ticket.fields.comment.comments
+    if STRICT:
+        comments = [each_comment for each_comment in comments if "q453337" in each_comment.body]
+    if TODAY:
+        comments = [each_comment for each_comment in comments if each_comment.updated.startswith(datetime.datetime.now().strftime("%Y-%m-%d"))]
+    if len(comments) > 0:
+        comments = sorted(comments, key=lambda x: x.updated, reverse=False)
+        results.append(Result(issue=ticket, comments=comments))
+
+results: List[Result] = sorted(results, key=lambda x: x.comments[len(x.comments)-1].updated, reverse=False)
+
+for result in results:
+    ticket: Issue = result.issue
+    comments: List[Comment] = result.comments
+
     print("##############################")
     print(remove_before_minus(ticket.key), ticket.fields.summary)
+    print(comments[len(comments)-1].updated)
     print("##############################")
-    comments = ticket.fields.comment.comments
-    sorted(comments, key=lambda x: x.updated, reverse=True)
     for each_comment in comments:
-        if each_comment.body.find("user-33") != -1:
-            print(f"{each_comment.updated}  -  {each_comment.author.displayName} \n--- \n {add_prefix_to_each_line(each_comment.body)}\n--------------")    
+        print(f"{each_comment.updated}  -  {each_comment.author.displayName} \n {add_prefix_to_each_line(each_comment.body)}\n--------------")    
