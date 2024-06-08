8238dab0f74a812a9f61a41b49db47e43cf35c02
Vitalii Cherkashyn <vitalii.cherkashyn@gmail.com>
2024-06-08 23:52:46 +0200

    anki update

--- html-automation/anki/anki-web.py
+++ html-automation/anki/anki-web.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import sys
 
@@ -7,17 +8,18 @@ import sys
 # open file and leave only column with word and translation
 # copy-paste to text file
 # give the name of that text file as an argument for current script
-# login and select your Desk
+# application will ask for login, select Desk, click button Add
+
 
 ATTEMPTS_COUNT=5
 
 
 def find_element_by_xpath(driver, xpath):
-	for _ in range(1,ATTEMPTS_COUNT):
-		try:
-			return driver.find_element_by_xpath(xpath)			
-		except:
-			continue
+    for _ in range(1,ATTEMPTS_COUNT):
+        try:
+            return driver.find_element(By.XPATH, xpath)
+        except:
+            continue
 
 
 def send_keys(element, text):
@@ -39,18 +41,32 @@ def click(element):
 if __name__=='__main__':
 	driver = webdriver.Firefox()
 	driver.get("https://ankiweb.net")
-	input("waiting for logging in and selecting Desk")
+	print("-----------------------------------------")
+	print("waiting for logging in")
+	print("selecting destination Desk")
+	input("click button Add and see 'UI for adding'")
 
-	front=find_element_by_xpath(driver, '//*[@id="f0"]')
-	back=find_element_by_xpath(driver, '//*[@id="f1"]')
-	save=find_element_by_xpath(driver, '/html/body/main/p/button')
-	if not front or not back or not save:
-		print("some of visual elements were not found")
+	front=find_element_by_xpath(driver, '/html/body/div/main/form/div[1]/div/div')
+	back=find_element_by_xpath(driver, '/html/body/div/main/form/div[2]/div/div')
+	save=find_element_by_xpath(driver, '/html/body/div/main/form/button')
+	if not front:
+		print("Front element was not found")
+		sys.exit(1)
+	if not back:
+		print("Back element was not found")
+		sys.exit(1)
+	if not save:
+		print("Add button was not found")
 		sys.exit(1)
 
 	with open(sys.argv[1], "r") as file:
 		for each_line in file:
-			word1, word2 = file.readline().split("\t")
+			words = each_line.split("\t")
+			if len(words) != 2:
+				input(f"---skip line: {each_line}")				
+				continue
+			word1 = words[0]
+			word2 = words[1]
 			word1 = word1.strip()
 			word2 = word2.strip()
 			print(word1, word2)
