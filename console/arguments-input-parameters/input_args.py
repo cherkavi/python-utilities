f67be6174a161b2623f153750b6a9f090ee37689
Voland <voland@nomail.com>
2025-11-24 11:41:53 +0100

    args with minus

--- console/arguments-input-parameters/input_args.py
+++ console/arguments-input-parameters/input_args.py
@@ -11,11 +11,19 @@ if __name__ == "__main__":
     parser.add_argument('--destination_user',
                         help='user of DB where script should be applied ',
                         required=False, default="default_schema")
-    
+    # recreate-db-user - will be converted to underscores
+    parser.add_argument('--recreate-db-user',   
+                        help='user will be re-created',
+                        required=False)
+
+
     # list of parameters like: --list 101 102 103 104
     parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', required=True)
     # you can use nargs='*' in case of 0..* parameters instead of nargs='+' that means 1..*
-    
+
     args = parser.parse_args()
 
-    main(args.version1, args.version2, args.recreate_db_user, args.create_user_sql, args.destination_user, True)
+    main(args.version1,
+         args.destination_user,
+         args.recreate_db_user,
+         True)
--- console/arguments-input-parameters/input_params.py
+++ console/arguments-input-parameters/input_params.py
@@ -3,8 +3,10 @@ import argparse
 parser = argparse.ArgumentParser(description="create interval set ")
     parser.add_argument("-c", "--config_name", required=True, help="Name of the YAML configuration file")
     parser.add_argument("--data_api_url", required=False, help="data-api url")
+    parser.add_argument("--data-api-port", required=False, help="data-api url")
     args = parser.parse_args()
 
     args.c
     args.config_name
     args.data_api_url
+    args.data_api_port
